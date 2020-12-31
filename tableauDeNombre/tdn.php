c65c25bedfbd2d5cc7830d07ee6205dc143b717f
jgcohen <75163282+jgcohen@users.noreply.github.com>
2020-12-31 16:39:05 +0100

    Number table

---
+++ tableauDeNombre/tdn.php
@@ -0,0 +1,25 @@
+<?php
+
+///creation of the table
+
+echo '<table border>
+<tr>
+    <th> Number </th>
+    <th> Square </th>
+    <th> Square Root </th>
+</tr>';
+
+/// We re gonna put numbers with a loop.
+
+for($i = 1; $i < 11; $i++){
+echo '  <tr>
+            <td>'.$i.' </td>
+            <td>'.$i*$i.' </td>
+            <td>'.sqrt($i).' </td>
+        </tr>';
+};
+
+
+////We close the table
+
+echo'</table>';
